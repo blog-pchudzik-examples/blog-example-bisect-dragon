2971dcaa70fe3c5ae7cb96eb19172fef83016581
Pawel Chudzik <pawel.chudzik@gmail.com>
2017-12-10 20:14:23 +0100

    added code

---
+++ project/src/main/java/com/pchudzik/blog/examples/bisect/dragon/Multiply.java
@@ -0,0 +1,13 @@
+package com.pchudzik.blog.examples.bisect.dragon;
+
+public class Multiply {
+	private final int number;
+
+	public Multiply(int number) {
+		this.number = number;
+	}
+
+	public int by(int other) {
+		return number * other;
+	}
+}
